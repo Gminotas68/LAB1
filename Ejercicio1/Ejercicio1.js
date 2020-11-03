76520081792c2699d69c9d49b64cc646e1170e92
Gustavo Adolfo Minotas Ruiz <guscolombiamex@gmail.com>
2020-11-03 18:31:23 -0500

    cambios en Java Script

--- Ejercicio1/Ejercicio1.js
+++ Ejercicio1/Ejercicio1.js
@@ -1,2 +1,18 @@
-Ejercicio1
+let N1;
+let N2;
+let N3;
+let N4;
+let N5;
+let Calificacion final;
 
+N1 = parcial 1 * 0,55;
+N2 = parcial 2 * 0,55;
+N3 = parcial 3 * 0,55;
+N4 = examen final * 0,30;
+N5 = trabajo final * 0,15;
+
+Calificación Final = (N1 + N2 + N3 + N4 + N5 / 5)
+
+Calificación Final = Number(prompt('ingrese calificacion parcial 1'));
+Number(prompt('ingrese calificacion parcial 2'));
+Number(prompt('ingrese calificacion parciales 3'));
